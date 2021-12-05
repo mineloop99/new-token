e98d89905b9acd89ce429490324030f6b933d4e9
mineloop99 <mineloop99@gmail.com>
2021-12-05 15:50:26 +0700

    python test deployment

--- tests/test_deploy_token.py
+++ tests/test_deploy_token.py
@@ -1,12 +1,36 @@
-from brownie import AniwarToken, networks, config
+from brownie import AniwarToken, network, config, exceptions, Contract
 from scripts.helper import get_account
+import pytest
 
 
 def test_deploy_token():
+    # Deploy Token
     account = get_account()
-    assert account.address == "0xca751C6800320e06180fA8a8266b17986b5E26d8"
     aniToken = AniwarToken.deploy(
         {"from": account},
-        publish_source=config["networks"][networks.show_active()]["verify"],
+        publish_source=config["networks"][network.show_active()]["verify"],
     )
-    assert aniToken.
+    # Transfer with paused()
+    aniToken.pause()
+    oldBalance = aniToken.balanceOf(account.address)
+    with pytest.raises(exceptions.VirtualMachineError):
+        tx = aniToken.transfer(
+            config["networks"][network.show_active()]["recipient"], 5000000
+        )
+        tx.wait(1)
+    assert aniToken.balanceOf(account.address) == oldBalance
+    # Transfer when unpaused()
+    oldBalance2 = aniToken.balanceOf(account.address)
+    aniToken.unpause()
+    tx = aniToken.transfer(
+        config["networks"][network.show_active()]["recipient"], 5000000
+    )
+    tx.wait(1)
+    assert aniToken.balanceOf(account.address) < oldBalance2
+
+
+def test_aniwar_farm():
+    # Get Contract
+    account = get_account()
+    aniToken = AniwarToken[-1]
+    print(aniToken)
