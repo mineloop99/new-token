501dd786b3cf47a57be5a0f4aa3ca16027a66741
huynhhung171099 <huynhhung171099@gmail.com>
2022-02-07 22:27:56 +0700

    storage

---
+++ contracts/NftStorage.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NftStorage is Ownable {
+    IERC721 private immutable _tokenContract;
+    struct Token {
+        bool isInit;
+        uint256 tokenId;
+    }
+    mapping(uint256 => address) private _nftDepositor;
+    mapping(uint256 => Token) private _nft;
+    uint256 private _tokenIndexCount = 0;
+
+    constructor(address tokenContract_) {
+        _tokenContract = IERC721(tokenContract_);
+    }
+
+    function deposit(uint256 _tokenId) public {
+        require(
+            _tokenContract.getApproved(_tokenId) == address(this),
+            "Not Approved"
+        );
+        _tokenContract.transferFrom(msg.sender, address(this), _tokenId);
+        _nftDepositor[_tokenId] = msg.sender;
+        _nft[_tokenId] = Token({isInit: true, tokenId: _tokenId});
+        _tokenIndexCount++;
+    }
+
+    function withdraw(uint256 _tokenIndex, address _withdrawer)
+        public
+        onlyOwner
+    {
+        _tokenContract.transferFrom(address(this), _withdrawer, _tokenIndex);
+        _nft[_tokenIndex].tokenId = _tokenIndexCount;
+        _tokenIndexCount--;
+    }
+}
